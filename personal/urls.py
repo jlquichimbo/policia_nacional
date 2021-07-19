a6f1c7fdf721d7b49f577a0f2a1b07acaaabcb86
jlquichimbo <joseluisquichimbo91@gmail.com>
2021-07-18 21:25:53 -0500

    Terminado  CRUD

--- personal/urls.py
+++ personal/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.persona_list, name='home'),
     path('persona_list', views.persona_list, name='persona_list'),
     path('persona_create', views.persona_create, name='persona_create'),
+    path('persona_detail/<pk>', views.persona_detail, name='persona_detail'),
+    path('persona_edit/<pk>', views.persona_edit, name='persona_edit'),
+    path('persona_delete/<pk>', views.persona_delete, name='persona_delete'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -32,3 +32,36 @@ def persona_create(request):
         form.save()
         return redirect('personal:persona_list')
     return render(request, 'personal/persona_create.html', context)
+
+
+# Ver detalle del empleado
+def persona_detail(request, pk):
+    persona = Persona.objects.get(pk = pk)
+    context = {
+        'persona': persona,
+    }
+    return render(request, 'personal/persona_detail.html', context)
+
+# Editar empleado
+def persona_edit(request, pk):
+    persona = Persona.objects.get(pk = pk)
+    form = PersonaForm(instance=persona)
+    context = {
+        'form': form,
+    }
+    if request.method == 'POST':
+        form = PersonaForm(request.POST, instance=persona)
+        form.save()
+        return redirect('personal:persona_list')
+    return render(request, 'personal/persona_create.html', context)
+
+# Eliminar empleado
+def persona_delete(request, pk):
+    persona = Persona.objects.get(pk = pk)
+    context = {
+        'persona': persona,
+    }
+    if request.method == 'POST':
+        persona.delete()
+        return redirect('personal:persona_list')
+    return render(request, 'personal/persona_delete.html', context)
